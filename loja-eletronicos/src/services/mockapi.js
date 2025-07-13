385f55e22a246d457f304a896302ed190c7cc04e
alvesmaarcos <alvesmarcos.es@gmail.com>
2025-07-13 15:28:33 -0300

    configurando ambiente de desenvolvimento

---
+++ loja-eletronicos/src/services/mockapi.js
@@ -0,0 +1,36 @@
+const API_URL = 'https://6873f534c75558e27355beac.mockapi.io/Produtos';
+
+export async function buscarProdutos() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    return data.map(item => ({
+        id: item.id,
+        nome: item.name || item.nome,
+        preco: item.price || item.preco,
+        categoria: item.categoria || 'Sem categoria',
+        estoque: item.estoque || 10,
+        disponivel: item.disponivel !== false
+    }));
+}
+
+//Cadastrar o produto
+export async function adicionarProduto(produto) {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: produto.nome,
+        price: produto.preco,
+        categoria: produto.categoria,
+        imagem: produto.imagem,
+        descricao: produto.descricao,
+        estoque: produto.estoque,
+        disponivel: produto.disponivel
+      })
+    });
+    
+    return await response.json();
+  }
